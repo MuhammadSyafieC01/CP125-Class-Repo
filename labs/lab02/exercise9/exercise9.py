631a7170630e792e20e80b3c6ff154c12b6b9df5
MuhammadSyafieC01 <monstared02@gmail.com>
2025-12-17 13:02:46 +0800

    push

--- labs/lab02/exercise9/exercise9.py
+++ labs/lab02/exercise9/exercise9.py
@@ -4,12 +4,14 @@
 def calculate_xp_required(current_level):
     """Return XP needed to level up (level * 100)."""
     # TODO: Implement this function
-    pass
+    return current_level * 100
 
 def can_level_up(xp_remaining, xp_required):
     """Return True if xp_remaining >= xp_required."""
     # TODO: Implement this function
-    pass
+    if xp_remaining >= xp_required:
+        return True
+    return False
 
 def simulate_leveling(total_xp):
     """
@@ -17,7 +19,11 @@ def simulate_leveling(total_xp):
     Returns: (final_level, remaining_xp)
     """
     # TODO: Implement using calculate_xp_required and can_level_up
-    pass
+    current_level = 1
+    xp_required = calculate_xp_required(current_level)
+
+    while can_level_up(total_xp,calculate_xp_required(current_level)):
+        print
 
 # Test your code here
 print("Testing Level Up Calculator...")
