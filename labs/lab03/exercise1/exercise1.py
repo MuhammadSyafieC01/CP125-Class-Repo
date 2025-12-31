c43d6487dd50cb03505c323a29269e1f6e72f97a
MuhammadSyafieC01 <monstared02@gmail.com>
2025-12-31 12:01:43 +0800

    update

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,13 @@
 def count_bright_spots(pixels):
-    pass
+    index = 0
+    while index < (len(pixels) - 1):
+        up_index 
+
+        if i > pixels[up_index] and i > pixels[down_index]:
+            print("Bright spot")
+        else:
+            print("Not a bright spot")
+
+Pixels = [100, 120, 200, 150, 180, 160, 140]
+
+count_bright_spots(Pixels)
