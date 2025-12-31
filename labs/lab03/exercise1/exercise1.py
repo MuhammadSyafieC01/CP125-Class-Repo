4de88f1f481035038712554cc1130bf167e9eb76
MuhammadSyafieC01 <monstared02@gmail.com>
2025-12-31 13:05:47 +0800

    push 31.12

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,7 +1,11 @@
 def count_bright_spots(pixels):
+    count = 0
 
+    for i in range (1,len(pixels)-1):
+        if pixels[i] > pixels[i+1] and pixels[i] > pixels[i-1]:
+            count += 1
+    print(count)
+    return count
 
-    
 Pixels = [100, 120, 200, 150, 180, 160, 140]
-
 count_bright_spots(Pixels)
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,16 @@
 def find_station(stations, name):
-    pass
+    if name in stations:
+        index = 0
+        for i in stations:
+            if name == i:
+                print(i)
+                print(index)
+                break
+            index +=1
 
 def count_stops(stations, start, stop):
     pass
+
+stations = ["Central", "Marina", "Bukit", "Orchard", "Sentosa"]
+
+find_station(stations,"Orchard")
