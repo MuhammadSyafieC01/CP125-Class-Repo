4cc761e0f4df6b22ec4ca9d35f9f506090d70cd6
MuhammadSyafieC01 <monstared02@gmail.com>
2026-04-01 12:57:52 +0800

    push

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -2,4 +2,28 @@ import pandas as pd
 
 
 def explore_data(filename):
-    pass
+    info = {"total_students":None,"subjects":None,"math_average":None,"highest_math_student":None}
+
+    df = pd.read_csv(filename)
+
+    size = df.shape
+    print(size)
+
+    subject = list(df.columns)
+    subject = [subject[2],subject[3],subject[4]]
+
+
+    info["total_students"] = size[0]
+    info["subjects"] = subject
+    info["math_average"] = float(df["Math"].mean())
+    info["highest_math_student"] = None
+    
+    print(df)
+    print("exit function")
+
+    return info
+
+
+
+result = explore_data("labs\lab09\data\students.csv")
+print(result)
